ab9a4cbe197038b136e4fc25745108f763f04da1
box-7 <hayainada1981@yahoo.co.jp>
2023-01-12 11:59:01 +0900

    password_digestカラム追加、各マイグレーションによりschema.rbの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230108130503) do
+ActiveRecord::Schema.define(version: 20230112025508) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20230108130503) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
