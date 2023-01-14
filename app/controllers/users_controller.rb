54cb7f74151364e5a3c010b566677139c6465bf6
box-7 <hayainada1981@yahoo.co.jp>
2023-01-15 01:40:53 +0900

    画像変更機能についてのコメント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,10 +28,14 @@ class UsersController < ApplicationController
   end
 
   def update
+    # def createとは異なり、find文が必要
     @user = User.find(params[:id])
     if @user.update_attributes(user_update_params)
+      # params[:image]ではなく、params[:user][:image]
+      # 画像の保存の書き方に注意
       if params[:user][:image]
           @user.image = "user_#{@user.id}.png"
+          # /public/user_images/の階層
           File.binwrite("public/user_images/#{@user.image}", params[:user][:image].read)
       end
       flash[:success] = "ユーザー情報を更新しました"
@@ -41,19 +45,6 @@ class UsersController < ApplicationController
     end
   end
 end
-    # # 引数はストロングパラメーターで、ユーザーオブジェクトを生成し、インスタンス変数に代入
-    # @user = User.new(user_params)
-    # @user.image = "user_images/default.png"
-    # if @user.save
-    #   log_in @user # 保存成功後、ログイン
-    #   flash[:success] = '新規作成に成功しました。' # フラッシュメッセージを渡す
-    #   # redirect_to user_url(@user) と同じ意味
-    #   redirect_to @user
-    # else
-    #   render :new
-    # end
-
-
 
   # ストロングパラメーター 入力できるカラムを制御する, :password_confirmation　.require(:user)
   def user_params
@@ -65,6 +56,7 @@ end
   end
 
   def user_update_params
+    # strongパラメーターに:imageも入れる
     params.require(:user).permit(:name, :email, :password, :password_confirmation,:self_introduction, :target_weight, :target_body_fat_percentage, :image)
   end
 
