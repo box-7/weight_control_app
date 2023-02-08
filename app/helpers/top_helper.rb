d0bbe61c1662cccf89dd3d71c9a02a004acc659b
box-7 <hayainada1981@yahoo.co.jp>
2023-02-09 01:11:51 +0900

    topページ css修正

--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -1,2 +1,9 @@
 module TopHelper
+    def weight_loss(article)
+        # グラフ生成のため月日の自動生成が走り、体重がnilのものも存在するため、.where.not(weight: nil)を指定する
+        oldest_weight = Article.where(user_id: article.user_id).where.not(weight: nil).order(created_at: :DESC).first
+
+        weight_loss = article.weight - oldest_weight.weight
+        return weight_loss.round(1)
+    end
 end
