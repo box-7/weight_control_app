51ac182be929d22b06a26c0ac6d73af5238a3b72
box-7 <hayainada1981@yahoo.co.jp>
2023-02-13 19:25:40 +0900

    Uglifier::Error:対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier # 変更前
+
+  config.assets.js_compressor = Uglifier.new(harmony: true)　# 変更後
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
