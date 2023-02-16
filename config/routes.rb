d225367812858d6619199f8d287859d100f43103
box-7 <hayainada1981@yahoo.co.jp>
2023-02-16 19:44:42 +0900

    ゲストログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  post   '/guest_login', to: 'sessions#guest_create'
 
   # メソッドなので、articleではなくpost、delete
   post 'like/:id' => 'likes#create', as: 'create_like'
