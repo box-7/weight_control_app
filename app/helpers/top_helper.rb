586cd6d7b04eb471ce08b7b02cd6fbfd9679df90
box-7 <hayainada1981@yahoo.co.jp>
2023-02-17 12:43:49 +0900

    top,articleのindexページで、from the biginningの値がおかしくなるのを修正

--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -1,7 +1,8 @@
 module TopHelper
     def weight_loss(article)
         # グラフ生成のため月日の自動生成が走り、体重がnilのものも存在するため、.where.not(weight: nil)を指定する
-        oldest_weight = Article.where(user_id: article.user_id).where.not(weight: nil).order(created_at: :DESC).first
+        # (created_at: :DESC)だと直近のものになってしまうため、(created_at: :ASC)
+        oldest_weight = Article.where(user_id: article.user_id).where.not(weight: nil).order(created_at: :ASC).first
 
         if article.weight && oldest_weight.weight
             weight_loss = article.weight - oldest_weight.weight
